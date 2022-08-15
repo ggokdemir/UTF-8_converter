77b39dc7306394f1d2eee86dec1ef5f62eac7390
Gurkan Gokdemir <68547461+ggokdemir@users.noreply.github.com>
2022-08-15 18:50:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 #manipulator.py Author:ggokdemir@splunk
 
 import csv
+import re
 import json
+from traceback import print_tb
 
 # May take logs directory as command input
 file_location = "MY_FILE_LOCATION"
 
-def row_to_json(row):
-    print('Inside of the row')
-    json_object = json.loads(row)
-    #json_object[]
+def search_link(row):
+    print('Inside of the row.')
+    #Find the link.
 
+    row_string = json.dumps(row)
+    
+    if(row_string.find("https://www.aljazeera.net/news/2")!= -1):
+        print(row_string[row_string.find("https://www.aljazeera.net/news/2"):row_string.find("reqHost")-6])
+    else:
+        print("Link not found.")
+        
+    print(" ")
+
+    
+def encoder(data):
+    return data.encode('cp1252').decode('cp1256')
+
+    
 def opener():   
     print('Inside of the opener.')
     with open('source.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            row_to_json(row)            
+            search_link(row)            
             #Instead of printing, rows are going to be saved as json objects.
+
 
 #main 
 opener()
+
+print(encoder("Óæí Ïæã ÈíåÞí"))
     
